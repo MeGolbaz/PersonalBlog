08c227efc2cc054fb977e7757b3f7c6e492c368a
Mehdi Golbaz <mehdigolbaz@Mehdis-MacBook-Pro.local>
2019-07-22 13:23:14 +0430

    First commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FooterWrapper } from './styled';
+
+const Footer = () => (
+  <FooterWrapper>
+    Theme by @
+    <a
+      href="https://github.com/wonism/gatsby-advanced-blog"
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      Mehdi
+    </a>
+  </FooterWrapper>
+);
+
+export default Footer;
