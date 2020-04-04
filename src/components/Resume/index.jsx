b87085e72af9d3d1cb0682e89f7c0a428fe7592b
Mehdi Golbaz <mehdigolbaz@mehdis-macbook-pro.home>
2020-04-04 12:16:39 +0200

    Update Images

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet';
 import { FaPrint, FaGithub, FaFacebook, FaTwitter, FaLinkedin } from 'react-icons/fa';
 import Clearfix from '~/components/Common/Clearfix';
 import { PREFIX, AUTHOR, EMAIL, GITHUB_ID, TWITTER_ID, FACEBOOK_ID, LINKEDIN_ID } from '~/constants';
-import * as profileUrl from '~/resources/me.png';
+import * as profileUrl from '~/resources/Me.jpg';
 import { Wrapper, BasicInformation, SocialInformation, MDInformation, Button } from './styled';
 
 const Resume = ({
