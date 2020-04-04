a6581d8859954c33a8024d5a027b744356814728
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2020-04-04 15:26:42 +0200

    Fix title css

--- src/components/Home/styled.js
+++ src/components/Home/styled.js
@@ -2,12 +2,6 @@ import styled from 'styled-components';
 
 export const Title = styled.h1`
   @import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro");
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
-  margin: auto;
   width: 80%;
   height: 1.5em;
   line-height: 1.5em;
@@ -15,7 +9,4 @@ export const Title = styled.h1`
   font-size: 61px;
   font-family: 'Source Sans Pro';
   text-align: center;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
 `;
